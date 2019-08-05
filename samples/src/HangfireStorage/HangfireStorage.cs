1892b5dda7b3666c73ef0068dbf15e61d0cec7aa
kangze <kangze25@hotmail.com>
2019-08-05 17:27:29 +0800

    init sample

--- samples/src/HangfireStorage/HangfireStorage.cs
+++ samples/src/HangfireStorage/HangfireStorage.cs
@@ -4,6 +4,7 @@ using System.Fabric;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Hangfire.ServiceFabric.Extensions;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
@@ -13,23 +14,12 @@ namespace HangfireStorage
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class HangfireStorage : StatefulService
+    internal sealed class HangfireStorage : HangfireStatefulService
     {
         public HangfireStorage(StatefulServiceContext context)
             : base(context)
         { }
 
-        /// <summary>
-        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
-        /// </summary>
-        /// <remarks>
-        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
-        /// </remarks>
-        /// <returns>A collection of listeners.</returns>
-        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
-        {
-            return new ServiceReplicaListener[0];
-        }
 
         /// <summary>
         /// This is the main entry point for your service replica.
--- src/HangFireStorageService/Extensions/HangfireStatefulService.cs
+++ src/HangFireStorageService/Extensions/HangfireStatefulService.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Hangfire.ServiceFabric.Extensions
 {
-    public class HangfireStatefulService : StatefulService
+    public abstract class HangfireStatefulService : StatefulService
     {
         public HangfireStatefulService(StatefulServiceContext context)
            : base(context)
@@ -29,39 +29,6 @@ namespace Hangfire.ServiceFabric.Extensions
         protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
         {
             return ServiceFabricStorageExtensions.CreateServiceReplicaListeners(this.StateManager);
-        }
-
-
-        /// <summary>
-        /// This is the main entry point for your service replica.
-        /// This method executes when this replica of your service becomes primary and has write status.
-        /// </summary>
-        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
-        protected override async Task RunAsync(CancellationToken cancellationToken)
-        {
-            // TODO: Replace the following sample code with your own logic 
-            //       or remove this RunAsync override if it's not needed in your service.
-
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
-
-            while (true)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-
-                using (var tx = this.StateManager.CreateTransaction())
-                {
-                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
-
-
-                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
-
-                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
-                    // discarded, and nothing is saved to the secondary replicas.
-                    await tx.CommitAsync();
-                }
-
-                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
-            }
         }
     }
 }
